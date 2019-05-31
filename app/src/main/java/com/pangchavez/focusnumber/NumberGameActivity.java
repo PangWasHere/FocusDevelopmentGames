e559542644d43f5cab5456a4fc544dd09d36069e
Farrah Marie Chavez <pangcaberte@gmail.com>
2019-05-31 13:46:49 +1200

    Created single activity for TwoNumberGame and FourNumberGame

---
+++ app/src/main/java/com/pangchavez/focusnumber/NumberGameActivity.java
@@ -0,0 +1,13 @@
+package com.pangchavez.focusnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NumberGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_game);
+    }
+}
